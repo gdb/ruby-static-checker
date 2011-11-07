8a7b1563dcdd4d5efc1c8e9bb86130d4bad73c80
Greg Brockman <gdb@gregbrockman.com>
2011-11-07 01:15:49 -0800

    Rename project

--- lib/ruby-checker.rb
+++
@@ -1,6 +0,0 @@
-$:.unshift(File.dirname(__FILE__))
-
-module RubyChecker
-end
-
-require 'ruby-checker/extn'
---
+++ lib/ruby-static-checker.rb
@@ -0,0 +1,6 @@
+$:.unshift(File.dirname(__FILE__))
+
+module RubyStaticChecker
+end
+
+require 'ruby-static-checker/extn'
