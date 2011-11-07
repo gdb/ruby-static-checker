e4034444c343bee9e4b29c502c03073bfb357010
Greg Brockman <gdb@gregbrockman.com>
2011-11-07 00:27:46 -0800

    Add missing lib directory

---
+++ lib/ruby-checker.rb
@@ -0,0 +1,6 @@
+$:.unshift(File.dirname(__FILE__))
+
+module RubyChecker
+end
+
+require 'ruby-checker/extn'
---
+++ lib/ruby-checker/extn.rb
@@ -0,0 +1,22 @@
+module Kernel
+  # from http://redcorundum.blogspot.com/2006/05/kernelqualifiedconstget.html
+  def fetch_class(str)
+    path = str.to_s.split('::')
+    from_root = path[0].empty?
+    if from_root
+      from_root = []
+      path = path[1..-1]
+    else
+      start_ns = ((Class === self) || (Module === self)) ? self : self.class
+      from_root = start_ns.to_s.split('::')
+    end
+    until from_root.empty?
+      begin
+        return (from_root+path).inject(Object) { |ns,name| ns.const_get(name) }
+      rescue NameError
+        from_root.delete_at(-1)
+      end
+    end
+    path.inject(Object) { |ns,name| ns.const_get(name) }
+  end
+end
